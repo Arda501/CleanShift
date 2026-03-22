4a782ea209fe34e0707de30a3075720cd1eb1b6c
Arda <ardaeska@gmail.com>
2026-03-22 02:17:09 +0100

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 4
-        versionName = "1.4"
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
